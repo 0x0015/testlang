6d5a962fd57285d31d3cb566c9336eddfa02b139
0x0015 <64482595+0x0015@users.noreply.github.com>
2025-03-09 21:08:11 -0400

    a little more work on minLang convert - still not quite there yet (soon)

--- minLangConvert/minLangConvert.cpp
+++ minLangConvert/minLangConvert.cpp
@@ -71,6 +71,7 @@ std::optional<minLang::ast::type> minLangConvertType(const ast::type& ty){
 			case ast::type::bool_type:
 				return minLang::ast::type{minLang::ast::type::bool_type};
 			default:
+				std::cerr<<"Error: During minLang conversion found unknown basic type"<<std::endl;
 				return std::nullopt;
 		}
 	}else if(std::holds_alternative<ast::type::array_type>(ty.ty)){
@@ -84,6 +85,68 @@ std::optional<minLang::ast::type> minLangConvertType(const ast::type& ty){
 }
 
 std::optional<minLang::ast::block> minLangConvertBlock(const ast::block& block){
+	minLang::ast::block output;
+
+	for(const auto& statement : block.statements){
+		if(std::holds_alternative<ast::block::declaration>(statement)){
+			const auto& decl = std::get<ast::block::declaration>(statement);
+			const auto& convTy = minLangConvertType(decl.ty);
+			if(!convTy)
+				return std::nullopt;
+			output.statements.push_back(minLang::ast::block::declaration{*convTy, decl.name});
+		}else if(std::holds_alternative<ast::block::assignment>(statement)){
+			const auto& asgn = std::get<ast::block::assignment>(statement);
+			const auto& convExp = minLangConvertExpr(asgn.assignFrom);
+			if(!convExp)
+				return std::nullopt;
+			output.statements.push_back(minLang::ast::block::assignment{asgn.assignTo, *convExp});
+		}else if(std::holds_alternative<ast::block::ifStatement>(statement)){
+			const auto& ifStat = std::get<ast::block::ifStatement>(statement);
+			const auto& ifBlockConv = minLangConvertBlock(*ifStat.ifBody);
+			if(!ifBlockConv)
+				return std::nullopt;
+			const auto& elseBlockConv = minLangConvertBlock(*ifStat.elseBody);
+			if(!elseBlockConv)
+				return std::nullopt;
+			const auto& condConv = minLangConvertExpr(ifStat.condition);
+			if(!condConv)
+				return std::nullopt;
+			output.statements.push_back(minLang::ast::block::ifStatement{*condConv, std::make_shared<minLang::ast::block>(*ifBlockConv), std::make_shared<minLang::ast::block>(*elseBlockConv)});
+		}else if(std::holds_alternative<ast::block::forStatement_normal>(statement)){
+			const auto& forStat = std::get<ast::block::forStatement_normal>(statement);
+			std::cerr<<"Error: not implementing this right now.  I can't think welll enough to wade through it (shouldn't be too hard)"<<std::endl;
+			return std::nullopt;
+		}else if(std::holds_alternative<ast::block::forStatement_while>(statement)){
+			const auto& whileStat = std::get<ast::block::forStatement_while>(statement);
+			const auto& bodyConv = minLangConvertBlock(*whileStat.body);
+			if(!bodyConv)
+				return std::nullopt;
+			const auto& condConv = minLangConvertExpr(whileStat.condition);
+			if(!condConv)
+				return std::nullopt;
+			output.statements.push_back(minLang::ast::block::forStatement_while{*condConv, std::make_shared<minLang::ast::block>(*bodyConv)});
+		}else if(std::holds_alternative<ast::block::returnStatement>(statement)){
+			const auto& ret = std::get<ast::block::returnStatement>(statement);
+			const auto& valConv = minLangConvertExpr(ret.val);
+			if(!valConv)
+				return std::nullopt;
+			output.statements.push_back(minLang::ast::block::returnStatement{*valConv});
+		}else if(std::holds_alternative<ast::expr>(statement)){
+			const auto& exp = std::get<ast::expr>(statement);
+			const auto& convExp = minLangConvertExpr(exp);
+			if(!convExp)
+				return std::nullopt;
+			output.statements.push_back(*convExp);
+		}else{
+			std::cerr<<"Error: During minLang conversion, encountered block statement of unknown type"<<std::endl;
+			return std::nullopt;
+		}
+	}
+
+	return output;
+}
+
+std::optional<minLang::ast::expr> minLangConvertExpr(const ast::expr& exp){
 	return std::nullopt;
 }
 
--- minLangConvert/minLangConvert.hpp
+++ minLangConvert/minLangConvert.hpp
@@ -3,6 +3,7 @@
 #include "../minLang/ast/ast.hpp"
 #include "../ast/ast.hpp"
 
+std::optional<minLang::ast::expr> minLangConvertExpr(const ast::expr& exp);
 std::optional<minLang::ast::block> minLangConvertBlock(const ast::block& block);
 std::optional<minLang::ast::type> minLangConvertType(const ast::type& ty);
 std::optional<minLang::ast::function> minLangConvertFunction(const ast::function& func);
